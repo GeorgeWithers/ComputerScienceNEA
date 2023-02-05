9195921d2ba371329ab013e389123f1fdb092928
GeorgeWithers <george.withers.2005@gmail.com>
2023-02-05 09:00:24 +0000

    !

--- BackEnd/server.py
+++ BackEnd/server.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def index():
     # List all the JSON files in the directory
-    json_files = [f for f in os.listdir() if f.endswith('.json')]
+    json_files = [f for f in os.listdir("/var/www/html/ComputerScienceNEA/FrontEnd/operatorsJson/") if f.endswith('.json')]
 
     # Return a JSON object containing the list of JSON files
     return jsonify({'files': json_files})
@@ -17,12 +17,38 @@ def index():
 @app.route('/<filename>', methods=['GET'])
 def get_data(filename):
     # Load the content of the JSON file into a Python object
-    with open(filename, 'r') as file:
+    with open("/var/www/html/ComputerScienceNEA/FrontEnd/operatorsJson." + filename, 'r') as file:
         data = json.load(file)
 
     # Return the content of the JSON file as a JSON object
     return jsonify(data)
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', 'http://127.0.0.1')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
 # Main function to run the Flask app
 if __name__ == '__main__':
     app.run(debug=True)
+
+from flask import Flask, make_response
+
+app = Flask(__name__)
+
+@app.after_request
+def set_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
+    return response
+
+@app.route('/')
+def index():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run()
---
+++ FrontEnd/OperatorCoder.py
@@ -0,0 +1,45 @@
+values_dict = {}
+
+while True:
+    main_value = input("Enter a value for the first dropdown (or press q to quit): ")
+    if main_value.lower() == "q":
+        break
+
+    sub_values = []
+    while True:
+        sub_value = input("Enter a subvalue for the second dropdown (or press q to quit): ")
+        if sub_value.lower() == "q":
+            break
+        sub_values.append(sub_value)
+
+    values_dict[main_value] = sorted(sub_values)
+
+# Save to HTML file
+with open("dropdowns.html", "w") as html_file:
+    html_file.write("<html>\n")
+    html_file.write("<body>\n")
+    html_file.write("<select id='firstDropdown' onchange='change_dropdown()'>\n")
+    for main_value in sorted(values_dict.keys()):
+        html_file.write(f"<option value='{main_value}'>{main_value}</option>\n")
+    html_file.write("</select>\n")
+    html_file.write("<select id='secondDropdown'>\n")
+    html_file.write("</select>\n")
+    html_file.write("<script>\n")
+    html_file.write("function change_dropdown() {\n")
+    html_file.write("var selectedValue = document.getElementById('firstDropdown').value;\n")
+    html_file.write("var secondDropdown = document.getElementById('secondDropdown');\n")
+    html_file.write("secondDropdown.innerHTML = '';\n")
+    for main_value in sorted(values_dict.keys()):
+        html_file.write(f"if (selectedValue === '{main_value}') {{\n")
+        for sub_value in values_dict[main_value]:
+            html_file.write(f"var option = document.createElement('option');\n")
+            html_file.write(f"option.value = '{sub_value}';\n")
+            html_file.write(f"option.text = '{sub_value}';\n")
+            html_file.write("secondDropdown.add(option);\n")
+        html_file.write("}\n")
+    html_file.write("}\n")
+    html_file.write("</script>\n")
+    html_file.write("</body>\n")
+    html_file.write("</html>\n")
+
+print("Values saved to dropdowns.html")
