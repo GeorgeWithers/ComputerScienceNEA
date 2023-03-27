87bd6feba9f215b02533f1c32c9d2084e10dab85
GeorgeWithers <george.withers.2005@gmail.com>
2023-03-27 20:24:43 +0100

    kjugkjg

--- BackEnd/newServer.py
+++ BackEnd/newServer.py
@@ -1,6 +1,34 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 import pandas as pd
-import astapp = Flask(__name__)
+import ast
+
+app = Flask(__name__)
 api = Api(app)
 
+app = Flask(__name__)
+
+# store 
+class Store(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('locationID', required=True, type=int)
+        parser.add_argument('name', required=True, type=int)
+        parser.add_argument('city', required=True, type=int)
+        args = parser.parse_args()
+        return{
+            'loc': args['locationID'],
+            'name': args['name'],
+            'city': args['city']
+        }, 200
+# load       
+class Load(Resource):
+    def get(self):
+        pass
+
+#API paths
+api.add_resource(Store, '/store/')
+api.add_resource(Load, '/load/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
