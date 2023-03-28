b4ee827aefaf84cc02c24ad6a9d0ba39c2beb66f
GeorgeWithers <george.withers.2005@gmail.com>
2023-03-28 17:41:28 +0100

    Getting ready to finally commit

--- BackEnd/newServer.py
+++ BackEnd/newServer.py
@@ -1,34 +1,18 @@
-from flask import Flask
-from flask_restful import Resource, Api, reqparse
-import pandas as pd
-import ast
-
-app = Flask(__name__)
-api = Api(app)
+import time
+from flask import Flask, request
 
 app = Flask(__name__)
 
-# store 
-class Store(Resource):
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('locationID', required=True, type=int)
-        parser.add_argument('name', required=True, type=int)
-        parser.add_argument('city', required=True, type=int)
-        args = parser.parse_args()
-        return{
-            'loc': args['locationID'],
-            'name': args['name'],
-            'city': args['city']
-        }, 200
-# load       
-class Load(Resource):
-    def get(self):
-        pass
+@app.route('/store', methods=['GET'])
+def store():
+    print("Hellp")
+    print(request.json.get('data'))
+    # timestamp = int(time.time())
+    # filename = f"data_{timestamp}.txt"
+    # with open(filename, 'w') as f:
+    #     f.write(data)
+    return('Data stored successfully in hello')
 
-#API paths
-api.add_resource(Store, '/store/')
-api.add_resource(Load, '/load/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- FrontEnd/createSpot.js
+++ FrontEnd/createSpot.js
@@ -1,41 +1,55 @@
-const createFile = async () => {
-  console.log('createFile function called');
-  const url = '/create_file';
-  const userId = 0000;
-  const numPlate = document.getElementById('number-plate').value;
-  const fleetNum = document.getElementById('fleet-number').value;
-  const operator = document.getElementById('operatorDropdown').value;
-  const region = document.getElementById('regionDropdown').value;
-  const route = document.getElementById('route').value;
-  const location = document.getElementById('location').value;
+var url = ":5000/store";
+// const createFile = async () => {
+//   console.log('createFile function called');
+//   const url = '/create_file';
+//   const userId = 0000;
+//   const numPlate = document.getElementById('number-plate').value;
+//   const fleetNum = document.getElementById('fleet-number').value;
+//   const operator = document.getElementById('operatorDropdown').value;
+//   const region = document.getElementById('regionDropdown').value;
+//   const route = document.getElementById('route').value;
+//   const location = document.getElementById('location').value;
 
-  console.log('Form values:', numPlate, fleetNum, operator, region, route, location);
+//   console.log('Form values:', numPlate, fleetNum, operator, region, route, location);
 
-  try {
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: `user_id=${userId}&num_plate=${numPlate}&fleet_num=${fleetNum}&operator=${operator}&region=${region}&route=${route}&location=${location}`
-    });
+//   try {
+//     const response = await fetch(url, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/x-www-form-urlencoded'
+//       },
+//       body: `user_id=${userId}&num_plate=${numPlate}&fleet_num=${fleetNum}&operator=${operator}&region=${region}&route=${route}&location=${location}`
+//     });
 
-    if (!response.ok) {
-      throw new Error('Failed to create file.');
-    }
+//     if (!response.ok) {
+//       throw new Error('Failed to create file.');
+//     }
 
-    const data = await response.text();
-    console.log('Response data:', data);
-  } catch (error) {
-    console.error('Error:', error);
-  }
-};
+//     const data = await response.text();
+//     console.log('Response data:', data);
+//   } catch (error) {
+//     console.error('Error:', error);
+//   }
+// };
 
 console.log('JavaScript code running');
 const createLogButton = document.getElementById('createLogButton');
 if (createLogButton) {
   createLogButton.addEventListener('click', () => {
     console.log('Button clicked');
+    var xhr = new XMLHttpRequest();
+xhr.open("POST", url);
+xhr.setRequestHeader("Accept", "application/json");
+xhr.setRequestHeader("Content-Type", "application/json");
+
+xhr.onreadystatechange = function () {
+   if (xhr.readyState === 4) {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+   }};
+
+xhr.send('{"data": 78912}');
     createFile();
   });
 }
+
